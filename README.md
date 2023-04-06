9be747276ff21c0618796faa01e361f039a8aedb
Akulen <tomas@rigaux.com>
2023-04-06 13:53:18 +0000

    Moved algo data to new database, and added single algo view

--- README.md
+++ README.md
@@ -28,6 +28,49 @@ the previously created venv, and point to `./app.wsgi`
 
 Then, edit `./app.wsgi` and change the path and secret key.
 
+## Initialize the database
+
+```bash
+flask shell
+```
+```python
+from app import *
+db.drop_all()
+db.create_all()
+```
+
+## Update the database structure
+
+Use this when the database structure was changed
+
+```bash
+flask db migrate -m "<Migration message>"
+```
+
+And this to apply the changes
+
+```bash
+flask db upgrade
+```
+
+## Add an element to the database
+
+```bash
+flask shell
+```
+```python
+from app import *
+cookie = Algo(name="ccgarden", short_title="Cookie Clicker Garden", title="Maximal number of unmarked cells with at least 3 marked neighboring cells in the n X n kings' graph", content='''{{ code('cookie.py') }}
+
+<p>
+Time complexity: \(O(m 2^{3n})\)<br />
+There is a solution in \(O(nm 2^{2n+3})\)
+The resulting sequences for \(N\\times N\) grids and \(N\\times(N+1)\) grids can be found at ??? and ???
+</p>''')
+db.session.add(cookie)
+db.session.commit()
+```
+
 ## Running the server for development
 
 ```bash
--- app.py
+++ app.py
@@ -1,6 +1,36 @@
+import os
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+        'sqlite:///' + os.path.join(basedir, 'database.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class Algo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False, unique=True)
+    short_title = db.Column(db.String(100), nullable=False)
+    title = db.Column(db.Text)
+    content = db.Column(db.Text)
+
+    def __repr__(self):
+        return f'<Algo {self.name}>'
+
+from flask import render_template_string
+from jinja2 import pass_context
+from markupsafe import Markup
+
+@app.template_filter()
+@pass_context
+def render_jinja(context, value):
+    return render_template_string(value, **context)
 
 @app.route("/")
 def index():
@@ -15,8 +45,14 @@ def cv():
     return render_template('cv.html')
 
 @app.route("/algo")
-def algo():
-    return render_template('algo.html')
+@app.route("/algo/<path:name>")
+def algo(name=None):
+    if name:
+        algo = Algo.query.filter_by(name=name).first()
+        if algo:
+            return render_template('algo_single.html', algo=algo)
+    algos = Algo.query.all()
+    return render_template('algo.html', algos=algos)
 
 @app.route("/sd")
 def sd():
